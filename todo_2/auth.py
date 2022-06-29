d9f9e3b715c6b30ec2b7ef070ebcfee78c06d43a
Alex Xiong <2428545784@qq.com>
2022-06-29 15:58:50 +1200

    add user login verification

--- todo_2/auth.py
+++ todo_2/auth.py
@@ -10,6 +10,24 @@ table_name = 'user'
 async def enter():
     return {'msg': 'hello world'}, 200
 
+@app.post("/login")
+async def verify_user(new_username, new_password):
+    connection = sqlite3.connect('todo.db')
+
+    cursor = connection.cursor()
+
+    sql_script = "select password from {} where name='{}'".format(db_name, new_username)
+    res = cursor.execute(sql_script)
+    save_password = res.fetchone()
+    connection.close()
+
+    if save_password:
+        save_password = save_password[0]
+    if save_password == new_password:
+        return {'msg': 'user login'}
+
+    return {'msg': 'user not exist or wrong password'}
+
 @app.post('/create/user')
 async def create_user(username, password):
     connection = sqlite3.connect(db_name)
